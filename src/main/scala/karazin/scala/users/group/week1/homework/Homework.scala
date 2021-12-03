62df4e6fbe423164041b3743efa3ae7cb35a94ee
Georgiy_Kozak <zhora.kozak2002@gmail.com>
2021-12-03 15:53:05 +0200

    Week-1 - Implementation of Boolean operators, Fermat numbers and Look-and-say Sequence

--- src/main/scala/karazin/scala/users/group/week1/homework/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week1/homework/Homework.scala
@@ -1,6 +1,7 @@
 package karazin.scala.users.group.week1.homework
 
 import scala.annotation.tailrec
+
 /**
  * Preface
  * Implement all the things with ???.
@@ -36,30 +37,71 @@ import scala.annotation.tailrec
  * For more details @see https://en.wikipedia.org/wiki/Kolakoski_sequence
  */
 
-object Homework :
+object Homework:
 
-  object `Boolean Operators` :
+  object `Boolean Operators`:
 
-    def not(b: Boolean): Boolean = ???
+    def not(b: Boolean): Boolean = if b then false else true
 
-    def and(left: Boolean, right: Boolean): Boolean = ???
+    def and(left: Boolean, right: Boolean): Boolean = if left then right else false
 
-    def or(left: Boolean, right: Boolean): Boolean = ???
+    def or(left: Boolean, right: Boolean): Boolean = if left then true else right
 
   end `Boolean Operators`
 
-  object `Fermat Numbers` :
+  object `Fermat Numbers`:
 
-    val multiplication: (BigInt, BigInt) => BigInt = ???
+    val multiplication: (BigInt, BigInt) => BigInt = (a, b) => {
+      @tailrec
+      def multImpl(a: BigInt, b: BigInt, acc: BigInt): BigInt = {
+        if b == 0 then acc
+        else multImpl(a, b - 1, acc + a)
+      }
 
-    val power: (BigInt, BigInt) => BigInt = ???
+      multImpl(a, b, 0)
+    }
 
-    val fermatNumber: Int => BigInt = ???
+    val power: (BigInt, BigInt) => BigInt = (a, n) => {
+      @tailrec
+      def powerImpl(a: BigInt, n: BigInt, acc: BigInt): BigInt = {
+        if n == 0 then acc
+        else powerImpl(a, n - 1, multiplication(acc, a))
+      }
+
+      powerImpl(a, n, 1)
+    }
+
+    val fermatNumber: Int => BigInt = n => power(2, power(2, n)) + 1
 
   end `Fermat Numbers`
 
-  object `Look-and-say Sequence` :
-    val lookAndSaySequenceElement: Int => BigInt = ???
+  object `Look-and-say Sequence`:
+
+    def toLookAndSay(curSequenceElement: BigInt): BigInt = {
+      //splitting a number into its digits
+      val list = curSequenceElement.toString().map(_.asDigit).toList
+
+      @tailrec
+      def implementation(digits: List[Int], curDigit: BigInt, count: BigInt, acc: BigInt): BigInt = {
+        if digits == List() then (acc * 10 + count) * 10 + curDigit //viewed the number
+        else {
+          if digits.head == curDigit then implementation(digits.tail, curDigit, count + 1, acc)
+          else implementation(digits.tail, digits.head, 1, (acc * 10 + count) * 10 + curDigit)
+        }
+      }
+
+      implementation(list.tail, list.head, 1, 0)
+    }
+
+    val lookAndSaySequenceElement: Int => BigInt = n => {
+      @tailrec
+      def loop(cur: BigInt, left: BigInt): BigInt = {
+        if left == 0 then cur
+        else loop(toLookAndSay(cur), left - 1)
+      }
+
+      loop(1, n)
+    }
 
   end `Look-and-say Sequence`
 
